183a190cd25fb7ed9dd6f7c97dc4f0a1c4740440
fireworks99 <46671672+fireworks99@users.noreply.github.com>
2025-10-15 14:36:56 +0800

    add vue-router dependency

--- docs/.vuepress/.temp/pages/post/09_Mybatis_core.html.js
+++ docs/.vuepress/.temp/pages/post/09_Mybatis_core.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/Code/SSMDocs/docs/.vuepress/.temp/pages/post/09_Mybatis_core.html.vue"
 const data = JSON.parse("{\"path\":\"/post/09_Mybatis_core.html\",\"title\":\"Mybatis核心组件\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Mybatis核心组件\",\"lang\":\"zh-CN\",\"author\":\"fireworks99\",\"date\":\"2025-04-23\"},\"headers\":[{\"level\":2,\"title\":\"核心组件\",\"slug\":\"核心组件\",\"link\":\"#核心组件\",\"children\":[]},{\"level\":2,\"title\":\"生命周期\",\"slug\":\"生命周期\",\"link\":\"#生命周期\",\"children\":[]},{\"level\":2,\"title\":\"实例\",\"slug\":\"实例\",\"link\":\"#实例\",\"children\":[{\"level\":3,\"title\":\"1.准备jar包\",\"slug\":\"_1-准备jar包\",\"link\":\"#_1-准备jar包\",\"children\":[]},{\"level\":3,\"title\":\"2.文件结构\",\"slug\":\"_2-文件结构\",\"link\":\"#_2-文件结构\",\"children\":[]},{\"level\":3,\"title\":\"3.输出\",\"slug\":\"_3-输出\",\"link\":\"#_3-输出\",\"children\":[]}]},{\"level\":2,\"title\":\"其他补充\",\"slug\":\"其他补充\",\"link\":\"#其他补充\",\"children\":[{\"level\":3,\"title\":\"纯代码创建SqlSessionFactory\",\"slug\":\"纯代码创建sqlsessionfactory\",\"link\":\"#纯代码创建sqlsessionfactory\",\"children\":[]},{\"level\":3,\"title\":\"Mapper+注解实现映射器\",\"slug\":\"mapper-注解实现映射器\",\"link\":\"#mapper-注解实现映射器\",\"children\":[]},{\"level\":3,\"title\":\"SqlSession直接发送SQL\",\"slug\":\"sqlsession直接发送sql\",\"link\":\"#sqlsession直接发送sql\",\"children\":[]}]}],\"git\":{\"updatedTime\":1745387289000,\"contributors\":[{\"name\":\"fireworks99\",\"username\":\"fireworks99\",\"email\":\"46671672+fireworks99@users.noreply.github.com\",\"commits\":1,\"url\":\"https://github.com/fireworks99\"}],\"changelog\":[{\"hash\":\"e22367e0aba2403a821a052465e9fce998457e54\",\"time\":1745387289000,\"email\":\"46671672+fireworks99@users.noreply.github.com\",\"author\":\"fireworks99\",\"message\":\"Mybatis\"}]},\"filePathRelative\":\"post/09_Mybatis_core.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
